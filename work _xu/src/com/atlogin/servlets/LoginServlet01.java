c06e8d3976ae8ef952f39560ab8114c51099f727
hhh21g <165292328+hhh21g@users.noreply.github.com>
2024-04-11 19:37:45 +0800

    Add files via upload

---
+++ work _xu/src/com/atlogin/servlets/LoginServlet01.java
@@ -0,0 +1,21 @@
+package com.atlogin.servlets;
+
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+public class LoginServlet01 extends HttpServlet {
+    @Override
+    protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+        req.setCharacterEncoding("UTF-8");
+        String name = req.getParameter("userName");
+        String password = req.getParameter("password");
+        String email = req.getParameter("email");
+
+        System.out.println(name);
+        System.out.println(password);
+        System.out.println(email);
+    }
+}
